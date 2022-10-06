652cae2e0d007fcfa23b9ee486d85db4aa7d61e1
omarjunior <omarhernandez041204@gmail.com>
2022-10-06 17:48:44 -0500

    cambios

--- configuracion/admin.py
+++ configuracion/admin.py
@@ -6,10 +6,36 @@ class TipoIdentificacionAdmin(admin.ModelAdmin):
     list_display = ('codigo', 'sigla', 'descripcion', 'activo', 'fecha_creacion')
     list_filter = ('descripcion',)
 
+class AsignaturaAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'nombre_asignatura', 'activo', 'fecha_creacion',)
+    list_filter = ('nombre_asignatura',)
+
+class GradoAdmin(admin.ModelAdmin):
+    list_display = ('grado', 'activo', 'fecha_creacion')
+    list_filter = ('grado',)
+
+class GrupoAdmin(admin.ModelAdmin):
+    list_display = ('get_grado', 'codigo', 'nombre_grupo', 'activo', 'fecha_creacion',)
+    list_filter = ('nombre_grupo',)
+
+    def get_grado(self, request):
+        if request.fk_grado:
+            return f"N° Grado: {request.fk_grado.grado}"
+        return ""
+    get_grado.short_description = "Grado"
+
+class DepartamentoAdmin(admin.ModelAdmin):
+    list_display = ('')
+    list_filter = ('')
+
+class MunicipioAdmin(admin.ModelAdmin):
+    list_display = ('',)
+    list_filter = ('',)
+
 
 admin.site.register(TipoIdentificacion, TipoIdentificacionAdmin)
-admin.site.register(Asignatura)
-admin.site.register(Grado)
-admin.site.register(Grupo)
+admin.site.register(Asignatura, AsignaturaAdmin)
+admin.site.register(Grado, GradoAdmin)
+admin.site.register(Grupo, GrupoAdmin)
 admin.site.register(Departamento)
 admin.site.register(Municipio)
